484befc190dac90a8803eb1205c752ede01f871a
Gaiokane <qk632773809@vip.qq.com>
2021-01-21 21:58:48 +0800

    调试完成，测试通过

--- src/test/java/com/example/test/TestApplicationTests.java
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -2,7 +2,7 @@ package com.example.test;
 
 import com.example.test.bean.UserBean;
 import com.example.test.service.UserService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
